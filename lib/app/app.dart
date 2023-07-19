514cb754f11d963b3d921ba964b069e543e75fa6
OsamaMa7rous <o.ma7rous10@gmail.com>
2023-07-19 23:31:09 +0300

    creating application class

---
+++ lib/app/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+   MyApp._internal();
+
+  static final MyApp instance = MyApp._internal();
+  factory MyApp() => instance;
+  int appState = 0;
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
