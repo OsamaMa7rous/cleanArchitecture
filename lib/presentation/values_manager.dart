a79345e24b30ebeff6c0001abe87ffbc6eec19f0
OsamaMa7rous <o.ma7rous10@gmail.com>
2023-07-20 05:25:00 +0300

    adding values manager

---
+++ lib/presentation/values_manager.dart
@@ -0,0 +1,32 @@
+class AppMargin{
+  static const m8 = 8.0;
+  static const m12 = 8.0;
+  static const m14 = 8.0;
+  static const m16 = 8.0;
+  static const m18 = 8.0;
+  static const m20 = 8.0;
+}
+
+class AppPadding{
+  static const p8 = 8.0;
+  static const p12 = 8.0;
+  static const p14 = 8.0;
+  static const p16 = 8.0;
+  static const p18 = 8.0;
+  static const p20 = 8.0;
+}
+
+
+class AppSize{
+  static const s8 = 8.0;
+  static const s12 = 8.0;
+  static const s14 = 8.0;
+  static const s16 = 8.0;
+  static const s18 = 8.0;
+  static const s20 = 8.0;
+  static const s28 = 8.0;
+  static const s60 = 8.0;
+  static const s65 = 8.0;
+}
+
+
