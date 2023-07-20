629ffab232738a3159e45fb15dd7a66198d5221f
OsamaMa7rous <o.ma7rous10@gmail.com>
2023-07-20 05:15:46 +0300

    adding fonts manager

--- lib/presentation/color_manager.dart
+++ lib/presentation/color_manager.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 class ColorManager {
-  static Color primaryColor = HexColor.fromHex("#ED9728");
+  static Color primary = HexColor.fromHex("#ED9728");
   static Color darkGery = HexColor.fromHex("#525252");
   static Color gery = HexColor.fromHex("#9E9E9E");
   static Color lightGrey = HexColor.fromHex("#707070");
+  static Color primaryOpacity70 = HexColor.fromHex("#B3ED9728");
 }
 
 extension HexColor on Color {
---
+++ lib/presentation/font_manager.dart
@@ -0,0 +1,24 @@
+import 'dart:ui';
+
+class FontConstants{
+
+  static const fontFamily = "Montserrat";
+}
+
+class FontWeightManger{
+static const light = FontWeight.w300;
+static const regular = FontWeight.w400;
+static const medium = FontWeight.w500;
+static const semiBold = FontWeight.w600;
+static const bold = FontWeight.w700;
+
+}
+class FontSize{
+  static const s12 = 12.0;
+  static const s14 = 14.0;
+  static const s16 = 16.0;
+  static const s17 = 17.0;
+  static const s18 = 18.0;
+  static const s20 = 20.0;
+
+}
